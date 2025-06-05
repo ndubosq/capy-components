e4ee3dcde054215620a12a8db7831c28ca7d977c
ndubosq <ndubosq@tetralink.fr>
2025-06-05 14:01:07 +0200

    wip

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from 'react'
-import QueryClientProvider from '@/app/1/_/query-client-provider'
+import QueryClientProvider from '@/app/issues/_/query-client-provider'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider>
       <NuqsAdapter>
